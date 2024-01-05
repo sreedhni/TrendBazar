b63dc29b5874c4fd2aa56ccd53eb5d49d1156997
sajay <sajaykannan10@gmail.com>
2024-01-05 12:55:43 +0530

    adding cors

--- trendBazar/settings.py
+++ trendBazar/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "store",
     "rest_framework",
      'rest_framework.authtoken',
+       "corsheaders",
      
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS=True
